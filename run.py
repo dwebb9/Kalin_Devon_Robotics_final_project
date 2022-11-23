efc36da90962cd77ac2f971c9d1bb70fc1da63b8
Devon <devon.webb@gmail.com>
2022-11-23 13:44:01 -0700

    ts

--- run.py
+++ run.py
@@ -53,95 +53,6 @@ def check_collision(q, obs_loc, obs_rad, testing=False):
                   return True
       return False
 
-def get_random_point():
-      # defining workspace as a sphere about (0,0,4) with radius 6
-      theta = np.random.random_sample()*2*np.pi
-      phi = np.random.random_sample()*np.pi
-      r = np.random.random_sample()*6
-      x = r * np.sin(phi) * np.cos(theta)
-      y = r * np.sin(phi) * np.sin(theta)
-      z = r * np.cos(phi) + 4
-      return [x,y,z]
-
-def find_closest_point(p, tree):
-      # print("p: ", p)
-      # print("tree: \n")
-      # for i in tree:
-      #       print(i.loc)
-
-      dist = 1000
-      closest = tree[0]
-      for i in tree:
-            newdist = np.linalg.norm(np.array(p) - np.array(i.loc))
-            if newdist < dist:
-                  closest = i
-                  dist = newdist
-      # print('closest: ', closest.loc)
-      return closest
-
-def compute_robot_path(q_init, goal, obst_location, obst_radius):
-      # this can be similar to your IK solution for HW 6, but will require modifications
-      # to make sure you avoid the obstacle as well.
-
-      #IK parameters
-      K = 0.4*np.eye(3,3)
-      ik_type = "p_inv"
-
-      # this is an empty list that we'll use to return our solutions so they can be plotted
-      q_s = []
-      searching = True
-      rrt_tree = []
-      starting_loc_T = arm.fk(q_init, arm.n)
-      starting_loc = starting_loc_T[0:3,3]
-      # starting_loc = np.array([0,0,4])
-
-      print("starting loc: ", starting_loc)
-      print("starting q: ", q_init)
-      rrt_tree.append(Node(starting_loc, q_init))
-      goalNode = Node(starting_loc, q_init)
-      segment_length = 0.4
-      last_jump = 0.6 #segment_length * 2
-      prog_dist = 10000
-      test_itt = 0
-      # check_collision(q_init, testing=True)
-      while searching: # set your st opping criteria here
-            p = get_random_point()
-            # p = np.array([0,2,4])
-            test_itt += 1
-            v_star = find_closest_point(p, rrt_tree) #closes point in tree
-            v_plus = (p - v_star.loc)*segment_length/np.linalg.norm(np.array(v_star.loc) - np.array(p)) + v_star.loc
-            (v_plus_q, ef, count, flag, message)  = arm.ik_position(v_plus, v_star.q, ik_type, K=K)
-            # print("\np: ", p)
-            # print("v_star: ", v_star.loc)
-            # print("v_plus: ", v_plus)
-            if not check_collision(v_plus_q, obst_location, obst_rad):
-                  # print("good points")
-                  newNode = Node(v_plus, v_plus_q)
-                  newNode.prev = v_star
-                  rrt_tree.append(newNode)
-                  # print("tree length: ", len(rrt_tree))
-                  dist_to_goal = np.linalg.norm(np.array(v_plus) - np.array(goal))
-                  if prog_dist > dist_to_goal:
-                        print("\nnew dist to goal: ", dist_to_goal)
-                        print("at : ", v_plus)
-                        prog_dist = dist_to_goal
-                  if dist_to_goal < last_jump:
-                        (goal_q, ef, count, flag, message)  = arm.ik_position(goal, newNode.q, ik_type, K=K)
-                        goalNode = Node(goal, goal_q)
-                        goalNode.prev = newNode
-                        rrt_tree.append(newNode)
-                        searching = False
-      
-      current = goalNode
-      print("path: ")
-      while current is not None:
-            q_s.append(current.q)
-            print(current.loc)
-            current = current.prev
-      
-      q_s.reverse()
-      return q_s
-
 if __name__ == "__main__":
 
       # if your function works, this code should show the goal, the obstacle, and your robot moving towards the goal.
@@ -150,17 +61,14 @@ if __name__ == "__main__":
       obst_position = [0, 3, 2]
       obst_rad = 1.0
 
-      q_ik_slns = compute_robot_path(q_0, goal, obst_position, obst_rad)
-
-
       # if you just want to check if you have your code set up correctly, you can uncomment the next three lines and run this file
       # using either vs code or the terminal (and running "python3 midterm_2022.py"). None of the next three lines are needed
       # for your solution, they may just help you check your visualization before you get going. It will just display 100 different
       # random sets of joint angles as well as the goal and obstacle.
 
       # import numpy as np
-      # q_ik_slns = np.random.uniform(size=(100,4))
-      # q_ik_slns = q_ik_slns.tolist()
+      q_ik_slns = np.random.uniform(size=(100,4))
+      q_ik_slns = q_ik_slns.tolist()
 
 
 
