0dab29e45f858bcade44a9f85aad2c42809d336c
Devon <devon.webb@gmail.com>
2022-11-29 16:28:21 -0700

    added 2d dh params

--- A_star.py
+++ A_star.py
@@ -28,45 +28,59 @@ def check_collision(q, obs_loc, obs_rad, arm):
       return False
 
 def get_astar_path():
-    #inputs: q0, obs_list(list of obstacles), grid_size, end_point
+      #inputs: q0, obs_list(list of obstacles), grid_size, end_point
 
-    #ouput: q_list(list of iterative q values)
-    q_list = []
+      #ouput: q_list(list of iterative q values)
+      q_list = []
+      # strat: similar to RRT solver. Find min path of tip of robot through environment, checking for colisions at each step. 
 
 
-    return q_list
+      return q_list
 
 if __name__ == "__main__":
-    from visualization import VizScene # this is the newest visualization file updated on Oct 12
-    import time
-    # if your function works, this code should show the goal, the obstacle, and your robot moving towards the goal.
-    q_0 = [0, 0, 0, 0]
-    goal = [0, 2, 4]
-    obst_position = [0, 3, 2]
-    obst_rad = 1.0
+      from visualization import VizScene # this is the newest visualization file updated on Oct 12
+      import time
+      # if your function works, this code should show the goal, the obstacle, and your robot moving towards the goal.
+      q_0 = [0, 0, 0, 0]
+      goal = [0, 2, 4]
+      obst_position = [0, 3, 2]
+      obst_rad = 1.0
 
-    # if you just want to check if you have your code set up correctly, you can uncomment the next three lines and run this file
-    # using either vs code or the terminal (and running "python3 midterm_2022.py"). None of the next three lines are needed
-    # for your solution, they may just help you check your visualization before you get going. It will just display 100 different
-    # random sets of joint angles as well as the goal and obstacle.
+      # if you just want to check if you have your code set up correctly, you can uncomment the next three lines and run this file
+      # using either vs code or the terminal (and running "python3 midterm_2022.py"). None of the next three lines are needed
+      # for your solution, they may just help you check your visualization before you get going. It will just display 100 different
+      # random sets of joint angles as well as the goal and obstacle.
 
-    # import numpy as np
-    q_ik_slns = np.random.uniform(size=(100,4))
-    q_ik_slns = q_ik_slns.tolist()
+      # import numpy as np
+      q_ik_slns = np.random.uniform(size=(100,3))
+      q_ik_slns = q_ik_slns.tolist()
 
+      #2D arm
+      dh = [[0, 0, 4, 0],
+            [0, 0, 4, 0],
+            [0, 0, 4, 0]]
+      arm = kin.SerialArm(dh)
 
+      #3D arm
+      # dh = [[np.pi/2.0, 4, 0, np.pi/2.0],
+      #       [np.pi/6.0, 0, 0, np.pi/2.0],
+      #       [0, 4.031, 0, np.pi/2.0],
+      #       [np.pi/6.0, 0, 2, np.pi/2.0]]
+      # arm = kin.SerialArm(dh)
 
+      # depending on how you store q_ik_slns inside your function, you may need to change this for loop
+      # definition. However if you store q as I've done above, this should work directly.
+      viz = VizScene()
 
-    # depending on how you store q_ik_slns inside your function, you may need to change this for loop
-    # definition. However if you store q as I've done above, this should work directly.
-    viz = VizScene()
-    viz.add_arm(arm, joint_colors=[np.array([0.95, 0.13, 0.13, 1])]*arm.n)
-    viz.add_marker(goal, size=20)
-    viz.add_obstacle(obst_position, rad=obst_rad)
-    for q in q_ik_slns:
-        viz.update(qs=[q])
+      viz.add_arm(arm, joint_colors=[np.array([0.95, 0.13, 0.13, 1])]*arm.n)
+      viz.add_marker(goal, size=20)
+      viz.add_obstacle(obst_position, rad=obst_rad)
 
-        # if your step in q is very small, you can shrink this time, or remove it completely to speed up your animation
-        time.sleep(0.1)
-        # time.sleep(0.05)
-    # viz.hold()
+      # viz.hold()
+      for q in q_ik_slns:
+            viz.update(qs=[q])
+
+            # if your step in q is very small, you can shrink this time, or remove it completely to speed up your animation
+            time.sleep(0.1)
+            time.sleep(0.05)
+      viz.hold()
